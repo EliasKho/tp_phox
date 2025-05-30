4ec926d91f545a9aa6ba8c974b0b398a82b7ef5a
EliasKho <144025058+EliasKho@users.noreply.github.com>
2025-05-30 18:37:15 +0200

    dans console : type + titre + descr
    affichage dans la console du type de l'image et de sa description

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ function getPicture(id){
     let promise = photoloader.loadPicture(id);
     promise.then(p=>{
         console.log(p.photo.titre);
+        console.log(p.photo.type);
+        console.log(p.photo.descr);
     })}
-//test github
 
 getPicture(window.location.hash ? window.location.hash.substr(1): 105);
+
+/*
+photo 105 :
+cats-796437_1920
+ image/jpeg
+ Rerum nulla numquam voluptatem. Suscipit consequuntur dicta error. Nisi dicta atque unde accusantium.
+
+photo 106 :
+ adresse : http://localhost/td6#106
+ cheetah-1170149_1920
+ image/jpeg
+ Velit consequatur molestiae commodi natus qui. Illo aut eaque maiores enim. Odit ex dolor vel id molestiae ut quia.
+*/
